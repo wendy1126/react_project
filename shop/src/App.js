70bd176a81564cf0a66e5afce9919b0702d76894
WENDY <97227279+wendy1126@users.noreply.github.com>
2023-02-13 09:48:48 +0900

    프로젝트 생성&부트스트랩 설치

---
+++ shop/src/App.js
@@ -0,0 +1,25 @@
+/*eslint-disable*/ //warning 메세지(lint) 끄는 기능
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Features</Nav.Link>
+            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
